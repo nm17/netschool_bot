6edba0eda35c4017ccbaf96f0784671f6de08863
nm17 <dannevergame@gmail.com>
2020-02-20 14:54:50 +0400

    fix: heroku deploy

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import os
+from pathlib import Path
+
 from kutana import Kutana, load_plugins
 from kutana.backends import Telegram
 import dotenv
 
-env = dotenv.dotenv_values()
+env = dotenv.dotenv_values() if Path(".env").exists() else os.environ
 
 # Create application
 app = Kutana()
 
 # Add manager to application
-app.add_backend(Telegram(env["TELEGRAM_TOKEN"], proxy="http://127.0.0.1:8118"))
+app.add_backend(Telegram(env["TELEGRAM_TOKEN"], proxy=env.get("PROXY")))
 
 # Load and register plugins
 app.add_plugins(load_plugins("plugins/"))
